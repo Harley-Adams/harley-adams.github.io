6476d7a5dfecc656642ae31150e183f08d1b3e9a
Harley Adams <harley.g.adams@gmail.com>
2023-10-02 22:55:06 -0700

    switch navbar to react core ui

--- web-ui/src/Navbar.tsx
+++ web-ui/src/Navbar.tsx
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { CContainer, CNavbar, CNavbarBrand } from "@coreui/react";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/">Profile</Link>
-        </li>
-        <li>
-          <Link to="/Leaderboard">Leaderboard</Link>
-        </li>
-        <li>
-          <Link to="/Posts">Posts</Link>
-        </li>
-      </ul>
-    </nav>
+    <CNavbar colorScheme="light" className="bg-light">
+      <CContainer fluid>
+        <CNavbarBrand className="mb-0 h1" href="/">
+          Profile
+        </CNavbarBrand>
+        <CNavbarBrand className="mb-0 h1" href="/Leaderboard">
+          Leaderboard
+        </CNavbarBrand>
+        <CNavbarBrand className="mb-0 h1" href="/Posts">
+          Posts
+        </CNavbarBrand>
+      </CContainer>
+    </CNavbar>
   );
 }
 export default Navbar;
