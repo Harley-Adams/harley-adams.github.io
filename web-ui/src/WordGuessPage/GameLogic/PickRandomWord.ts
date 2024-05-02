e1d6899c78471fb7b20d4063e795f1ff1d04bbb4
Harley Adams <harley.g.adams@gmail.com>
2024-05-01 19:01:33 -0700

    use a better wordlist

--- web-ui/src/WordGuessPage/GameLogic/PickRandomWord.ts
+++ web-ui/src/WordGuessPage/GameLogic/PickRandomWord.ts
@@ -1,6 +1,11 @@
 import { Dictionary } from "../Data/Dictionary";
+import { FiveLetterWordList } from "../Data/FiveLetterWordList";
 
 export const PickRandomWord = (wordLength: number): string => {
+  if (wordLength == 5) {
+    let randomIndex = Math.floor(Math.random() * FiveLetterWordList.length);
+    return FiveLetterWordList[randomIndex].toUpperCase();
+  }
   const potentialWords = Dictionary.filter(
     (word) => word.length === wordLength
   );
